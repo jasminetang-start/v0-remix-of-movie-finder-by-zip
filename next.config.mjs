4548c37312bbafe0f9cb252dc2d90b675956153f
huangf1 <huangf1@rose-hulman.edu>
2025-10-25 16:12:34 -0700

    deployment fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',  // static export
+  basePath: '/movie-finder-by-city',
   eslint: {
     ignoreDuringBuilds: true,
   },
