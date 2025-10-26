92e6cfed4169c3e8acbb980826debb8f725b23c7
huangf1 <huangf1@rose-hulman.edu>
2025-10-26 14:55:42 -0700

    fix image path

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function getAssetPath(path: string) {
+  const basePath = '/movie-finder-by-city'
+  return `${basePath}${path}`
+}
