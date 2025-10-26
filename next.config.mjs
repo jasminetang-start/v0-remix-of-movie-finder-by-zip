180d5bd3aa75c1992e12ad02ce322e9baec78485
huangf1 <huangf1@rose-hulman.edu>
2025-10-26 14:44:35 -0700

    fix css loading issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/movie-finder-by-city',
+  assetPrefix: '/movie-finder-by-city/', 
   eslint: {
     ignoreDuringBuilds: true,
   },
